5f2c6f746722a2da8684305a6987775321fe608e
kaktusvason <fobrukekay@outlook.com>
2023-04-06 14:00:01 +0200

    Add support for multiple consensus mechanisms

--- token_contract_templates.go
+++ token_contract_templates.go
@@ -1,3 +1,4 @@
 // Add support for ERC-721A tokens
 // Add support for fractional token ownership
 // Add support for decentralized credit scoring
+// Add support for decentralized lending platforms
