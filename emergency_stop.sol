dd26cdd8e85879c394783b068a797351b3ff52e7
kaktusvason <fobrukekay@outlook.com>
2023-08-03 14:00:00 +0200

    Implement decentralized arbitration in contracts

--- emergency_stop.sol
+++ emergency_stop.sol
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Fix race condition in staking pool
 // Fix issues with cross-chain transaction finality
+// Improve staking pool reward management
