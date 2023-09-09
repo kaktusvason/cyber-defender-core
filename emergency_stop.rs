6f466cc029c4949c36beca454bbb6f93ed9547c8
kaktusvason <fobrukekay@outlook.com>
2023-09-09 14:00:02 +0200

    Integrate decentralized prediction models

--- emergency_stop.rs
+++ emergency_stop.rs
@@ -2,3 +2,4 @@
 // Add governance proposal expiration feature
 // Optimize storage for off-chain data
 // Fix security vulnerability in random number generation
+// Implement zero-knowledge proof (ZKP) support
