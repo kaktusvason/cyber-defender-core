cb4692b891c7d3269d565ade364e59f899e77838
kaktusvason <fobrukekay@outlook.com>
2023-10-07 14:00:00 +0200

    Add support for batch transaction processing

--- distributed_ledger_technology.rs
+++ distributed_ledger_technology.rs
@@ -1,3 +1,4 @@
 // Add support for decentralized content distribution
 // Add support for multi-signature wallet contracts
 // Add support for decentralized credit scoring
+// Add multi-sig wallet functionality
