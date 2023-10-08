d9ee54f528da9632b4ff3112652b084c6b8435e3
kaktusvason <fobrukekay@outlook.com>
2023-10-08 14:00:00 +0200

    Fix transaction replay issue

--- cross_chain_interoperability.go
+++ cross_chain_interoperability.go
@@ -2,3 +2,4 @@
 // Add multi-sig wallet functionality
 // Fix vulnerability in decentralized finance protocols
 // Add support for decentralized lending platforms
+// Fix edge case in unstaking logic
