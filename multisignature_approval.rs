ed6fb1b3670e4e7db7ad9d653abeee63453f5129
kaktusvason <fobrukekay@outlook.com>
2023-10-10 14:00:00 +0200

    Enhance governance contract flexibility

--- multisignature_approval.rs
+++ multisignature_approval.rs
@@ -2,3 +2,4 @@
 // Fix issues with gas optimization in smart contracts
 // Improve smart contract code modularity
 // Fix issue with multi-chain transactions
+// Improve staking pool reward management
