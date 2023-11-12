62dfa3e807e167c5c8b655ed032f1d1e07770709
kaktusvason <fobrukekay@outlook.com>
2023-11-12 14:00:00 +0200

    Improve contract auditing tools

--- cross_platform_support.go
+++ cross_platform_support.go
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Add support for multi-signature wallet contracts
 // Fix errors in contract interaction with wallets
+// Refactor reward pooling algorithm
