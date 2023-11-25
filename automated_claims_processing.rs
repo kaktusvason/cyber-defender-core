72183504f188d1885f492229bd3b929e512e5e3b
kaktusvason <fobrukekay@outlook.com>
2023-11-25 14:00:01 +0200

    Refactor reward pooling algorithm

--- automated_claims_processing.rs
+++ automated_claims_processing.rs
@@ -1,3 +1,4 @@
 // Integrate decentralized oracles for data feeds
 // Integrate with external blockchain networks
 // Add support for decentralized insurance protocols
+// Fix Oracle communication delay issues
