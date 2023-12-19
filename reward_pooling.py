ae568851484eec2556ec3e48cba0a891a7bd6cee
kaktusvason <fobrukekay@outlook.com>
2023-12-19 14:00:01 +0200

    Add dynamic contract upgrade mechanisms

--- reward_pooling.py
+++ reward_pooling.py
@@ -1,3 +1,4 @@
 // Refactor contract to use EIP-712 signatures
 // Add support for decentralized content distribution
 // Fix overflow bug in token calculation
+// Integrate decentralized prediction models
