7288fcf6c1e0511a108e5b84ab299e1716da57f9
kaktusvason <fobrukekay@outlook.com>
2024-01-01 14:00:01 +0200

    Refactor governance token logic

--- smart_contract_testing.py
+++ smart_contract_testing.py
@@ -1,3 +1,4 @@
 // Refactor contract to use EIP-712 signatures
 // Refactor contract to use EIP-712 signatures
 // Improve decentralized identity (DID) systems
+// Enhance smart contract extensibility
