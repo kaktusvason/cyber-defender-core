2830fe2586eea4e3c6fbba5a7180461385dab32c
kaktusvason <fobrukekay@outlook.com>
2024-02-23 14:00:00 +0200

    Fix contract reentrancy issues

--- staking_incentives.go
+++ staking_incentives.go
@@ -2,3 +2,4 @@
 // Integrate with decentralized identity providers
 // Refactor decentralized exchange (DEX) code
 // Implement reward distribution feature
+// Implement on-chain fee adjustment mechanisms
