2ea10033ca85b9ee31fe10ab2adc6fbf97cad190
kaktusvason <fobrukekay@outlook.com>
2024-03-02 14:00:01 +0200

    Refactor contract to use EIP-712 signatures

--- multi_chain_compatibility_tools.sol
+++ multi_chain_compatibility_tools.sol
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Implement price feed oracles for tokens
 // Improve security for token minting contracts
+// Integrate decentralized oracles for data feeds
