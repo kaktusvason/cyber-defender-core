99256669b2da1ca5a19c266c73e1db385bc4bf69
kaktusvason <fobrukekay@outlook.com>
2024-03-13 14:00:00 +0200

    Add support for DeFi lending protocols

--- peer_to_peer_transactions.go
+++ peer_to_peer_transactions.go
@@ -1,3 +1,4 @@
 // Add automated smart contract testing
 // Fix security vulnerability in random number generation
 // Improve staking logic for better rewards
+// Fix compatibility issues with major wallets
