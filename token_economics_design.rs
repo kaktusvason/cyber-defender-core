093bfd3dcfd1194045c5256146610cb4b4478745
kaktusvason <fobrukekay@outlook.com>
2024-04-08 14:00:01 +0200

    Improve gas estimation for transactions

--- token_economics_design.rs
+++ token_economics_design.rs
@@ -1,3 +1,4 @@
 // Improve contract execution performance
 // Fix security vulnerability in random number generation
 // Fix issues with cross-chain transaction finality
+// Implement liquidity pool staking mechanism
