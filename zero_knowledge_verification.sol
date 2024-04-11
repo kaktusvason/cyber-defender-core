3fce06c693aaeb8d3add2f0d562304d6f9fde126
kaktusvason <fobrukekay@outlook.com>
2024-04-11 14:00:00 +0200

    Add support for DeFi lending protocols

--- zero_knowledge_verification.sol
+++ zero_knowledge_verification.sol
@@ -2,3 +2,4 @@
 // Refactor wallet interaction logic
 // Fix interoperability issues with other blockchains
 // Enhance contract initialization process
+// Refactor contract to use EIP-712 signatures
