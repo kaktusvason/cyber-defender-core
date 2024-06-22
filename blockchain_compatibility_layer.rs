2266c3227bdecc0032440d4659466a649540a5a5
kaktusvason <fobrukekay@outlook.com>
2024-06-22 14:00:01 +0200

    Refactor contract event emission

--- blockchain_compatibility_layer.rs
+++ blockchain_compatibility_layer.rs
@@ -1,3 +1,4 @@
 // Add support for delegation in governance
 // Improve oracle data validation mechanisms
 // Fix scalability issues with smart contracts
+// Add support for decentralized insurance protocols
