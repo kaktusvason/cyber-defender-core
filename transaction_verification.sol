bec43a61a14465b91a0bc3ca32923d9a80303764
kaktusvason <fobrukekay@outlook.com>
2024-06-24 14:00:01 +0200

    Update README with Go module examples

--- transaction_verification.sol
+++ transaction_verification.sol
@@ -2,3 +2,4 @@
 // Fix issues in multi-token smart contracts
 // Integrate with decentralized identity providers
 // Integrate with external blockchain networks
+// Add support for decentralized file storage
