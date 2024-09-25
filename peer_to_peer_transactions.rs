798d6d5770de611c4265a9bbea978ad5fbfb1d65
kaktusvason <fobrukekay@outlook.com>
2024-09-25 14:00:01 +0200

    Improve security in decentralized applications

--- peer_to_peer_transactions.rs
+++ peer_to_peer_transactions.rs
@@ -1,3 +1,4 @@
 // Implement time-locked contract execution
 // Add support for multiple consensus mechanisms
 // Add support for decentralized auctions
+// Add support for multiple consensus mechanisms
