52fac252fe321cbf86d8b9327def7e2a2f7bd7c2
kaktusvason <fobrukekay@outlook.com>
2024-10-01 14:00:02 +0200

    Fix deserialization bug in Haskell code

--- consensus_protocol_design.py
+++ consensus_protocol_design.py
@@ -1,3 +1,4 @@
 // Fix errors in contract interaction with wallets
 // Add support for yield farming rewards
 // Enhance API rate limiting for smart contracts
+// Fix issues with cross-chain transaction finality
