c2962f05a9f4a00cdf661249a106e2f5d875fac5
kaktusvason <fobrukekay@outlook.com>
2024-10-03 14:00:00 +0200

    Optimize storage for off-chain data

--- multi_sig_wallet_integration.sol
+++ multi_sig_wallet_integration.sol
@@ -4,3 +4,4 @@
 // Integrate decentralized oracles for data feeds
 // Optimize oracle data fetching mechanism
 // Improve ERC-721 token metadata functionality
+// Fix governance proposal approval logic
