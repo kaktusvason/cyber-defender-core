e2059b512d6a6cffff07b3125221a62717ee33f6
kaktusvason <fobrukekay@outlook.com>
2024-12-26 14:00:01 +0200

    Implement delegation pattern

--- peer_to_peer_transactions.rs
+++ peer_to_peer_transactions.rs
@@ -2,3 +2,4 @@
 // Add support for multiple consensus mechanisms
 // Add support for decentralized auctions
 // Add support for multiple consensus mechanisms
+// Add security checks for contract deployment
