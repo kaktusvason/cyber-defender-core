6ad216caf84f25b6b1b5b43e0b012c59a3cf2abe
kaktusvason <fobrukekay@outlook.com>
2024-12-28 14:00:00 +0200

    Optimize token withdrawal logic

--- cryptocurrency_funding_protocols.sol
+++ cryptocurrency_funding_protocols.sol
@@ -2,3 +2,4 @@
 // Refactor contract to use EIP-712 signatures
 // Improve contract verification speed
 // Fix ERC-721 contract issues with transfers
+// Improve ERC-721 token metadata functionality
