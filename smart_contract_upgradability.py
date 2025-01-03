fa05dced4b85ff6102f5f80b3c0870c9146bb492
kaktusvason <fobrukekay@outlook.com>
2025-01-03 14:00:01 +0200

    Refactor staking logic

--- smart_contract_upgradability.py
+++ smart_contract_upgradability.py
@@ -4,3 +4,4 @@
 // Add support for multiple consensus mechanisms
 // Fix issue with multi-chain transactions
 // Improve security for token minting contracts
+// Fix issues with gas optimization in smart contracts
